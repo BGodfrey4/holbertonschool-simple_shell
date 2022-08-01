e8d67c10f59ab3c1cfb1058adce31fa8326cf500
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2022-08-01 00:09:21 -0500

    Update hsh.c

--- hsh.c
+++ hsh.c
@@ -1,1 +1,16 @@
+/**
+* main - the simple shell
+* @argc: is the number of arguments
+* @argv: is the array of arguments
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
 
+
+        if (!argc)
+        return (0);
+        prompt(argv[0]);
+        return (0);
+}
